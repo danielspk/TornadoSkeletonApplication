cb65ace60ed09865ec2029c81f4482aaf9e02622
Daniel M. Spiridione <daniel.spiridione@gmail.com>
2015-04-20 20:22:52 -0300

    Configuración mediante array

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,9 @@
 
 define('URLFRIENDLY', '/');
 
-$app->config('tornado_environment_development', true);
-$app->config('tornado_hmvc_use', true);
-$app->config('tornado_hmvc_module_path', 'app/modules');
-$app->config('tornado_hmvc_serialize_path', 'app/modules');
+$app->config([
+    'tornado_environment_development' => true,
+    'tornado_hmvc_use'                => true,
+    'tornado_hmvc_module_path'        => 'app/modules',
+    'tornado_hmvc_serialize_path'     => 'app/modules'
+]);
